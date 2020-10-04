3b0974c1e77d6647b29cd8f819a54167bfb286a6
caiocsgomes <caiocsgomes@gmail.com>
2020-10-04 19:03:58 -0300

    Adding front page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      'ubuntu': ['ubuntu', 'sans-serif']
+    },
+    gridTemplateColumns: {
+      'main': '200px auto'
+    }
+  },
+  variants: {},
+  plugins: [],
+}
